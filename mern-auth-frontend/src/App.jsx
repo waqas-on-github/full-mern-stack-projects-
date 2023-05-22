7dd1ce7a025c37aca296dca460fe0990e77de623
waqas-on-github <waqasvu892@gmail.com>
2023-05-22 16:59:57 -0400

    front end setup completed

---
+++ mern-auth-frontend/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+import Header from './components/Header'
+import Home from './screens/Home'
+
+
+function App() {
+ 
+
+  return (
+ <> 
+   <Header/>
+  <Home/>
+ 
+ </>
+    
+      
+
+  )
+}
+
+
+
+
+
+export default App
