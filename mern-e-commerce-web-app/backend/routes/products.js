0c284ba0bb534f3614e9cae7723d7f8c297a0fcd
waqas-on-github <waqasvu892@gmail.com>
2023-06-20 20:19:44 +0500

    products route implemented

--- mern-e-commerce-web-app/backend/routes/products.js
+++ mern-e-commerce-web-app/backend/routes/products.js
@@ -1,20 +1,28 @@
 import { Router } from 'express'
-
-import { products } from '../prd.js'
+import { Product } from '../models/productModel.js'
+import expressAsyncHandler from 'express-async-handler'
 
 
 const router = Router()
 
 // GET localhost:3000/products
-router.get('/', function(req, res) {
-  res.json(products)
-})
-
-
-router.get('/:id' , (req, res) => {
-const product = products.find((item ) => item._id === req.params.id)
+router.get('/', expressAsyncHandler(  async function   (req, res) {
+  const product = await  Product.find()
   res.json(product)
-})
+}))
+
+
+router.get('/:id', expressAsyncHandler(async (req, res) => {
+  console.log();
+  const singleproduct = await Product.findById(req.params.id); // Remove the conversion to String
+  console.log(singleproduct);
+  if (singleproduct) {
+    res.json(singleproduct);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+}));
+
 
 
 export { router }
---
+++ mern-e-commerce-web-app/backend/seeder.js
@@ -0,0 +1,54 @@
+import 'dotenv/config.js'
+import   './config/db.js'
+import mongoose from "mongoose";
+import users from "./data/user.js";
+import products from "./data/products.js";
+import {User} from   './models/Usersmodel.js'
+import {Product} from  './models/productModel.js'
+import { Order  } from "./models/orderModel.js";
+
+
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
+
+    const adminUser = createdUsers[0]._id
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data Imported!')
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`)
+    process.exit(1)
+  }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === '-d') {
+  destroyData()
+} else {
+  importData()
+}
