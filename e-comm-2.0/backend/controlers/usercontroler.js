826880d9c58cb0a1748539de61c7b6b7e363de43
waqas-on-github <waqasvu892@gmail.com>
2023-07-05 20:47:50 +0500

    signup login logout implemented with jwt

--- e-comm-2.0/backend/controlers/usercontroler.js
+++ e-comm-2.0/backend/controlers/usercontroler.js
@@ -1,81 +1,117 @@
 import { User } from "../models/user.schmea.js"
+import CustomError from "../utils/customError.js"
+import asynchandler from "../services/asynchandler.js"
 
 
+const signup  = asynchandler(  async (req, res) =>  {
+  const cookieoptions = { 
+    httpOnly : true   , 
+    expires :new Date (Date.now()+ 3*24*60*60*1000 )
 
-async  function  createuser    (req, res) {
+  }
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
 
-    try {
-       const user = await User.create(req.body) 
-   
-       res.status(201).json({
-         user
-       })
-    } catch (error) {
-     res.status(500).json({
-       error : error.message
-     })
-    }
+  // get data from user
+   const {name , email ,password}   = req.body
+  // validation  
+   if((!name || !email ||  !password)) {
+    throw new  CustomError("please add all fields" , 400 )
    }
 
-
-
- async  function getAllUsers  (req, res ) {
+   const userexist = User.findOne({email})
+   if(userexist) {
+    throw new  CustomError ("user already exist " , 400 )
+   }
+  //  lets add this data to db 
    
-try {
-      const user = await User.find() 
+    const user = User.create({
+      name  ,  email, password
+    })
+    const token = user.getJWTtoken()
+  //  safety 
+  user.password =undefined
+  res.cookies("token" , token  , cookieoptions)
+
+ return  res.status(201).json({
+    sucess  : true , 
+    token , 
+    user
+   })
+
+  })
+
+/*********************************************************
+  * @LOGIN
+  * @route http://localhost:5000/api/auth/login
+  * @description User Login Controller for signing in the user
+  * @returns User Object
+  *********************************************************/
+
+ 
+const login  = asynchandler(async (req, res) => {
+ 
+ const {email, password} = req.body
+    if(!email || !password) {
+     throw new  CustomError("please provide all fields" , 500 )
+    }
+   
+  const user = user.findOne({email}).select("+password") 
   if(!user) {
-     return  res.status(500).json({
-        sucess : false , 
-        error : " no users found"
-    })
+    throw new  CustomError("invalid craditionals" , 400)
   }
 
-      res.status(200).json({
-        sucess : true ,
-        user
-      })
-
-
-} catch (error) {
-    res.status(500).json({
-        sucess :  false  , 
-        error   : error.message 
-    })
-}
-
- } 
-
-
-async function getsingleuser (req, res) {
-  const id = req.params.id    
-  try {
-    const user = await User.findById(id) 
-    if(!user){
-       return res.status(500).json({
-            sucess : false , 
-            error : "no users found"
-        })}
-     
-        res.status(200).json({
-            sucess : true ,
-            user
-    })
-
-  } catch (error) {
-    res.status(500).json({
-        sucess :  false  , 
-        error   : error.message 
-    })
+  const ispasswordmatched = user.comparepass(password)
+  if(ispasswordmatched) {
+    const token = user.getJWTtoken()
+     user.password = undefined , 
+     res.cookies('token' , token , cookieoptions) ;
+   return res.status(201).json({
+      sucess  : true , 
+      token , 
+      user
+     })
   }
+throw  new CustomError('wrong password' , 400)
+   
+
+ } )
+
+/**********************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User Logout Controller for logging out the user
+ * @description Removes token from cookies
+ * @returns Success Message with "Logged Out"
+ **********************************************************/
+
+ const logout = asynchandler(async (req, res) => {
+  res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true
+  })
+
+  res.status(200).json({
+      success: true,
+      message: 'Logged Out'
+  })
+})
 
 
 
-}
+  
+
+
+
 
 
 
 export {
-    createuser ,
-     getAllUsers, 
-     getsingleuser
+     signup ,
+     login, 
+     logout
    }
---
+++ e-comm-2.0/backend/services/asynchandler.js
@@ -0,0 +1,13 @@
+const asynchandler  = (fn) => async function (req, res, next) {
+    try {
+        await fn(req , res , next )
+    } catch (error) {
+        res.status(error.code || 500).json({
+            sucess : false , 
+            message : error.message
+        })
+    }
+} 
+
+
+export default  asynchandler
